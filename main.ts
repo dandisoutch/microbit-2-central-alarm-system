ad70968f23251aa48a9e71ebc2f4b320d1029dcb
dandisoutch <124478672+dandisoutch@users.noreply.github.com>
2024-09-15 20:01:15 +0200

    La till funktionen i en forever loop så att det är async när man tar emot signal

--- main.ts
+++ main.ts
@@ -1,8 +1,6 @@
-function vait () {
-    // Skickar signal till Microbit 3
-    radio.sendString("ALARM_IS_ON")
+function distress_signal () {
     time_passed = control.millis()
-    while (control.millis() - time_passed < 10000 && alarm) {
+    while (control.millis() - time_passed < 30000 && alarm) {
         music.play(music.tonePlayable(175, music.beat(BeatFraction.Whole)), music.PlaybackMode.InBackground)
         basic.showLeds(`
             # # # # #
@@ -15,13 +13,15 @@ function vait () {
         basic.pause(200)
     }
     if (alarm) {
+        radio.setGroup(66)
         // Skickar Signal till Microbit 4 (police)
         radio.sendString("INTRUSION_DETECTED")
+        basic.showIcon(IconNames.Sad)
+        radio.setGroup(65)
     }
 }
 input.onButtonPressed(Button.A, function () {
-    alarm = true
-    vait()
+    radio.sendString("TURN_ON/OFF")
 })
 radio.onReceivedString(function (receivedString) {
     // Microbit 3 sätter på eller stänger av systemet
@@ -33,13 +33,12 @@ radio.onReceivedString(function (receivedString) {
         } else {
             turned_on = true
         }
-    } else if (receivedString == "DETECTED") {
+    } else if (receivedString == "DETECTED" && turned_on) {
         alarm = true
-        vait()
     }
 })
 input.onButtonPressed(Button.B, function () {
-    alarm = false
+    radio.sendString("DETECTED")
 })
 let time_passed = 0
 let alarm = false
@@ -47,3 +46,8 @@ let turned_on = false
 turned_on = false
 alarm = false
 radio.setGroup(65)
+basic.forever(function () {
+    if (alarm) {
+        distress_signal()
+    }
+})
